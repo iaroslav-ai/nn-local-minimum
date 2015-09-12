4af251ded1229c09bdf6442f95f1830a8567c4f1
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2015-09-12 23:42:30 +0200

    Update code

--- LocalMinimaDemo/Demo2D.py
+++ LocalMinimaDemo/Demo2D.py
@@ -1,15 +1,26 @@
+"""
+This code computes the gradient descent for Gaussian mixture model and plots
+result of fitting 
+"""
+
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-3, 3, 200)
-y = np.exp( -(x ** 2)/2  ) - np.exp( -(x ** 2)  )*2 + np.exp( -(x ** 2)*4  ) *2.5- np.exp( -(x ** 2)*8  )*1.6
+y = np.exp( -(x ** 2)/2  ) - np.exp( -(x ** 2)  )*2 + np.exp( -(x ** 2)/0.25  ) *2.5- np.exp( -(x ** 2)/0.125  )*1.6
 
-# multiplier / offset / output weights
-# bad initialization
-W = np.array([ [1,1,1,1], [-2,-1,1,2], [1,1,1,1] ])
-# initialization that leads to global optimum
-#W = np.array([ [0.7,1,2,5], [0,0,0,0], [1,-2,2.5,-1.6] ])
+# deviation / mean / output weights
+#W = np.array([ [0.2], [1.2], [1] ])# best model by enumeration (by hand :D )
+W = np.array([ [1,0.3,0.3,1], [-2,-0.3,0.3,2], [1,1,1,1] ])# bad initialization
+#W = np.array([ [1.7,1,0.5,0.35], [0,0,0,0], [1,-2,2.5,-1.6] ])# initialization that leads to global optimum
+#W = np.array([ [1,0.3,0.3,1, 0.3, 0.3, 0.2], [-2,-0.3,0.3,2, -0.7, 0.7, 0], [1,1,1,1,1,1,1] ])# better results with more neurons
+
+# Here I translate the parameters to more useful form
+tW = np.abs(W[0,])
+W[0,] = 1 / tW
+W[1,] = W[1,] / tW
+
 shapeOfW = W.shape
 W0 = np.squeeze(np.asarray(W))
 
@@ -25,10 +36,11 @@ def Objective(Win):
 
 sol = opt.minimize(Objective, W0)
 W = Unflatten(sol.x)
+print "solution objective:", Objective(W)
 
 yp = ComputeGaussians(W, x);
 
-fig,ax = plt.subplots()
+fig,ax = plt.subplots(figsize=(10,4))
 # plot data and predictions
 ax.plot(x, y,'*')
 ax.plot(x, yp,'-')
@@ -37,7 +49,7 @@ xp = W[1,] / W[0,] # location of Gaussian
 
 ax.plot(xp, xp*0,'o')
 
-# plot gaussians, if necessary
+# plot separate gaussians, if necessary
 
 """
 for i in range(4):
